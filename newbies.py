584528309899faa5ea6d2615ccfc42610751128d
andreaowu <andreaowu@berkeley.edu>
2016-02-18 18:53:34 -0800

    Commented code

--- newbies.py
+++ newbies.py
@@ -11,73 +11,115 @@ api_key = "7b4d4b32c605d5f4a7a6484a77231d"
 root_url = "https://api.meetup.com/20LetsHang/events/"
 keysign = "&key=" + api_key + "&sign=true"
 attendance_url = "/attendance?" + keysign
-event_host = dict() # maps event id to the host(s) id
-attended = dict() # maps people to who first timers they brought
-morethanone = []
-g = ""
-vid = dict()
-total = 0
+event_host = dict()             # key: event id, value: list of host name(s)
+attended = dict()               # key: host name, value: list of first timers' names
+events_per_member = dict()      # key: member name, value: number of events attended
+morethanone = []                # names of members who have been to more than one event
+g = ""                          # graph that we will draw out at the end
+vid = dict()            
+total_events = 0                # total number of events for 20LH
 
+'''
+Input:  uid is the meetup event id
+        index is the number of people who attended events we so far parsed
+
+Output: total accumulation of attendees
+
+Function also:  adds attendees' names to attended dict
+                adds new vertices to g for attendees we haven't seen before
+                adds edges to g between hosts and newbies
+
+'''
 def get_attendance_for_event(uid, index):
-    "Gets the people who attended for given id meetup event"
+    "Returns the number of attendees for given uid of a meetup event"
     url = root_url + str(uid) + attendance_url + keysign
     u = urllib.urlopen(url)
     try:
         info = json.load(u)
         for i in info:
             if i['status'] == "attended":
-                if i['member']['name'].strip() == u'T\xf9ng N\u1edd D\xea':
-                    i['member']['name'] = u'Tung No De'
-                elif i['member']['name'].strip() == u'R\xe9mi':
-                    i['member']['name'] = u'Remi'
+                name = i['member']['name'].encode("utf-8").strip()
 
-                if i['member']['name'].strip() not in attended:
-                    attended[i['member']['name'].strip()] = []
-                    g.add_vertex(i['member']['name'])
-                    vid[i['member']['name'].strip()] = index
+                """ Processes newbies """
+                if name not in attended and name != 'Former member':
+                    attended[name] = []     
+                    g.add_vertex(name)
+                    vid[name] = index
                     index += 1
+
+                    """ Goes through the host list for this event
+                        and draws an edge between each host and each
+                        attendee
+                        Updates list of newbies """
                     for h in event_host[uid]:
-                        g.add_edge(vid[h], vid[i['member']['name'].strip()])
+                        g.add_edge(vid[h], vid[name])
                         new = list(attended[h])
-                        new.append(i['member']['name'].strip())
+                        new.append(name)
                         attended[h] = new
+
+                if name in events_per_member:
+                    events_per_member[name] = events_per_member[name] + 1
+                else:
+                    events_per_member[name] = 1
+
     except ValueError:
         print "ValueError for " + url + " !"
+
     return index
 
+"""
+Input:  none
+Output: none
+
+Function also:  gets event_id with member_name
+                makes vertices for hosts who have never attended an event prior to hosting
+                tracks members who have been to more than one event
+"""
 def get_all_events():
     "Gets all the event id's in LetsHang along with its host"
+
     index = 0
-    all_events_url = "https://api.meetup.com/2/events?&photo-host=public&group_urlname=20LetsHang&fields=event_hosts,id&status=past" + keysign
+    all_events_url = ("https://api.meetup.com/2/events?&photo-host=public"
+                     "&group_urlname=20LetsHang&fields=event_hosts," 
+                     "id&status=past" + keysign)
     info = json.load(urllib.urlopen(all_events_url))
+
+    """ Parsing through each event """
     for i in info['results']:
-        global total 
-        total += 1
+
+        global total_events 
+        total_events += 1
+        event_id = i['id']
+
         if "event_hosts" in i:
+
+            """ Parsing through each event host for specific event """
             for h in i['event_hosts']:
-                if i['id'] not in event_host:
-                    event_host[i['id']] = [h['member_name'].strip()]
+
+                member_name = h['member_name'].encode().strip()
+
+                """ Updates event_host dict with name of member who hosted 
+                    event with event_id """
+                if event_id not in event_host:
+                    event_host[event_id] = [member_name]
                 else:
-                    new = list([event_host[i['id']]])
-                    new.append(h['member_name'].strip())
-                    event_host[i['id']] = new
+                    new = list([event_host[event_id]])
+                    new.append(member_name)
+                    event_host[event_id] = new
 
-                if h['member_name'].strip() == u'T\xf9ng N\u1edd D\xea':
-                    h['member_name'] = u'Tung No De'
-                elif h['member_name'].strip() == u'R\xe9mi':
-                    h['member_name'] = u'Remi'
-
-                if h['member_name'].strip() not in attended:
-                    g.add_vertex(h['member_name'].strip())
-                    vid[h['member_name'].strip()] = index
+                """ Checking just in case member hosted event without attending
+                    one before; need to start new vertex in graph """
+                if member_name not in attended:
+                    g.add_vertex(member_name)
+                    vid[member_name] = index
                     index += 1
-                    attended[h['member_name'].strip()] = []
+                    attended[member_name] = []
                 else:
-                    if h['member_name'].strip() not in morethanone:
-                        morethanone.append(h['member_name'].strip())
+                    if member_name not in morethanone:
+                        morethanone.append(member_name)
 
-                index = get_attendance_for_event(i['id'], index)
-
+                index = get_attendance_for_event(event_id, index)
+        
 def number():
     "Shows how many newbies each member got"
     for key, value in attended.iteritems():
@@ -92,19 +134,21 @@ def alphabatize():
         temp = [key,value]
         dictlist.append(temp)
     dictlist.sort()
-    dictlist = re.sub("\', \[u\'", ": ", str(dictlist))
-    dictlist = re.sub("\', \[\]\], \[u\'", "\n", str(dictlist))
-    dictlist = re.sub("\'\]\], \[u\'", "\n", str(dictlist))
-    dictlist = re.sub("\', u\'", ", ", str(dictlist))
+    dictlist = re.sub("\', \[\'", ": ", str(dictlist))
+    dictlist = re.sub("\', \[\]\], \[\'", "\n", str(dictlist))
+    dictlist = re.sub("\'\]\], \[\'", "\n", str(dictlist))
+    dictlist = re.sub("\', \'", ", ", str(dictlist))
     dictlist = re.sub("\', \[\]\]\]", "", str(dictlist))
-    dictlist = re.sub("\[\[u\'", "", str(dictlist))
+    dictlist = re.sub("\[\[\'", "", str(dictlist))
     f = open("/Users/andreawu/Documents/meetupnewbies/members.txt", "w")
     f.write("Total number of distinct attendees: " + str(len(vid.keys())) + "\n")
-    f.write("Total number of events: " + str(total) + "\n\n")
+    f.write("Total number of events: " + str(total_events) + "\n\n")
     f.write("Total number of distinct attendees who have been to more than 1 event: " \
         + str(len(morethanone)) + "\n")
-    morethanone_str = re.sub("u\'", "\'", str(morethanone))
+    morethanone_str = re.sub("\'", "\'", str(morethanone) + "\n")
     morethanone_str = re.sub("\'", "", morethanone_str)
+    morethanone_str = re.sub("\[", "", morethanone_str)
+    morethanone_str = re.sub("\]", "", morethanone_str)
     f.write("Attendees who have been to more than 1 event: " + morethanone_str)
     f.write(str(dictlist))
     f.close()
